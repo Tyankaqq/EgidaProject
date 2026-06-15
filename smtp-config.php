c8714753474cce5ede5a1d17c4b8c6800f380169
Tyankaqq <Tyankaqq@outlook.com>
2026-06-15 20:39:11 +0200

    bug fixs

---
+++ smtp-config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'host' => 'smtp.masterhost.ru',
+    'port' => 465,
+    'secure' => 'ssl', // ssl for 465, tls for 587
+    'username' => 'info@egida39.ru',
+    'password' => 'n6LlEeZCXf',
+    'from_email' => 'info@egida39.ru',
+    'from_name' => 'Сайт ЮК Эгида',
+    'to_email' => 'info@egida39.ru',
+    'to_name' => 'ЮК Эгида',
+];
